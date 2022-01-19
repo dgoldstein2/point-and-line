130f10f9ad6dd9af4088ad8f40d8a51c35a2346d
dgoldstein2 <dgoldstein0406@gmail.com>
2022-01-19 17:11:36 -0500

    worked on it a bit

--- LineProject/TestLine2D.java
+++ LineProject/TestLine2D.java
@@ -1,5 +1,7 @@
 package LineProject;
 
 public class TestLine2D {
-    
+    public static void main(String[] args){
+        
+    }
 }
--- PointProject/Point.java
+++ PointProject/Point.java
@@ -30,15 +30,33 @@ public class Point {
         
     }
     public boolean isCollinear(Point p1, Point p2){
-        double f = (double)(positiony)/(double)(positionx);
-        double z = (double)(p1.getY())/(double)(p1.getX());
-        double b = (double)(p2.getY())/(double)(p2.getX());
-        if ((f == z) && (z == b)){
+        double f1 = (double)(positiony)-(double)(p1.getY());
+        System.out.println(f1);
+
+        double z1 = (double)(positionx)-(double)(p1.getX());
+        System.out.println(z1);
+        double b1 = f1/z1;
+        double f2 = (double)(positiony)-(double)(p2.getY());
+        System.out.println(f2);
+        double z2 = (double)(positionx)-(double)(p2.getX());
+        System.out.println(z2);
+        double b2 = f2/z2;
+        double f3 = (double)(p2.getY())-(double)(p1.getY());
+        System.out.println(f3);
+        double z3 = (double)(p2.getX())-(double)(p1.getX());
+        System.out.println(z3);
+        double b3 = f2/z2;
+
+
+        System.out.println(b1 + " " + b2 + " " + b3);
+        return false;
+        /*if (((f - z < .01) && (f - z > -.01) && (z - b < .01) && (z - b > -.01) && (b - f < .01) && (b - f > -.01))){
             return true;
         }
         else{
             return false;
         }
+        */
     }
 
 }
